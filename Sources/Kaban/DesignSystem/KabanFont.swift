8b7c06d81cd06c623d4eb39d05f5f9aa1eb996e0
maiyama <myam0118@gmail.com>
2026-05-06 13:58:04 +0900

    テキストスタイルの等幅フォント対応とログ機能削除

--- Sources/Kaban/DesignSystem/KabanFont.swift
+++ Sources/Kaban/DesignSystem/KabanFont.swift
@@ -18,53 +18,97 @@ public enum KabanFontWeight {
     }
 }
 
+/// Font design token used by Kaban text styles.
+public enum KabanFontDesign: Sendable {
+    /// Default system font design.
+    case `default`
+    /// Monospaced system font design.
+    case monospaced
+
+    internal var swiftUIFontDesign: Font.Design {
+        switch self {
+        case .default: .default
+        case .monospaced: .monospaced
+        }
+    }
+}
+
 /// Dynamic Type-aware font token used by Kaban components.
 public struct KabanFont: Sendable {
     internal let size: CGFloat
     internal let relativeTo: Font.TextStyle
     internal let weight: Font.Weight
+    internal let design: KabanFontDesign
     internal let lineSpacing: CGFloat
 
-    private init(size: CGFloat, relativeTo: Font.TextStyle, weight: KabanFontWeight, lineSpacing: CGFloat) {
+    private init(
+        size: CGFloat,
+        relativeTo: Font.TextStyle,
+        weight: KabanFontWeight,
+        design: KabanFontDesign,
+        lineSpacing: CGFloat
+    ) {
         self.size = size
         self.relativeTo = relativeTo
         self.weight = weight.swiftUIFontWeight
+        self.design = design
         self.lineSpacing = lineSpacing
     }
 
     /// Large title style.
-    public static func titleLarge(weight: KabanFontWeight = .bold) -> KabanFont {
-        KabanFont(size: 32, relativeTo: .title, weight: weight, lineSpacing: 4)
+    public static func titleLarge(
+        weight: KabanFontWeight = .bold,
+        design: KabanFontDesign = .default
+    ) -> KabanFont {
+        KabanFont(size: 32, relativeTo: .title, weight: weight, design: design, lineSpacing: 4)
     }
 
     /// Regular title style.
-    public static func titleRegular(weight: KabanFontWeight = .bold) -> KabanFont {
-        KabanFont(size: 26, relativeTo: .title2, weight: weight, lineSpacing: 3)
+    public static func titleRegular(
+        weight: KabanFontWeight = .bold,
+        design: KabanFontDesign = .default
+    ) -> KabanFont {
+        KabanFont(size: 26, relativeTo: .title2, weight: weight, design: design, lineSpacing: 3)
     }
 
     /// Small title style.
-    public static func titleSmall(weight: KabanFontWeight = .bold) -> KabanFont {
-        KabanFont(size: 20, relativeTo: .title3, weight: weight, lineSpacing: 3)
+    public static func titleSmall(
+        weight: KabanFontWeight = .bold,
+        design: KabanFontDesign = .default
+    ) -> KabanFont {
+        KabanFont(size: 20, relativeTo: .title3, weight: weight, design: design, lineSpacing: 3)
     }
 
     /// Large body style.
-    public static func bodyLarge(weight: KabanFontWeight = .regular) -> KabanFont {
-        KabanFont(size: 17, relativeTo: .body, weight: weight, lineSpacing: 3)
+    public static func bodyLarge(
+        weight: KabanFontWeight = .regular,
+        design: KabanFontDesign = .default
+    ) -> KabanFont {
+        KabanFont(size: 17, relativeTo: .body, weight: weight, design: design, lineSpacing: 3)
     }
 
     /// Regular body style.
-    public static func bodyRegular(weight: KabanFontWeight = .regular) -> KabanFont {
-        KabanFont(size: 15, relativeTo: .subheadline, weight: weight, lineSpacing: 3)
+    public static func bodyRegular(
+        weight: KabanFontWeight = .regular,
+        design: KabanFontDesign = .default
+    ) -> KabanFont {
+        KabanFont(size: 15, relativeTo: .subheadline, weight: weight, design: design, lineSpacing: 3)
     }
 
     /// Small body style.
-    public static func bodySmall(weight: KabanFontWeight = .regular) -> KabanFont {
-        KabanFont(size: 13, relativeTo: .footnote, weight: weight, lineSpacing: 2)
+    public static func bodySmall(
+        weight: KabanFontWeight = .regular,
+        design: KabanFontDesign = .default
+    ) -> KabanFont {
+        KabanFont(size: 13, relativeTo: .footnote, weight: weight, design: design, lineSpacing: 2)
     }
 
     /// Caption style.
-    public static func captionRegular(weight: KabanFontWeight = .regular) -> KabanFont {
-        KabanFont(size: 11, relativeTo: .caption2, weight: weight, lineSpacing: 2)
+    public static func captionRegular(
+        weight: KabanFontWeight = .regular,
+        design: KabanFontDesign = .default
+    ) -> KabanFont {
+        KabanFont(size: 11, relativeTo: .caption2, weight: weight, design: design, lineSpacing: 2)
     }
 }
 
@@ -79,15 +123,17 @@ extension View {
 
 private struct KabanFontModifier: ViewModifier {
     private let weight: Font.Weight
+    private let design: Font.Design
 
     @ScaledMetric private var scaledSize: CGFloat
 
     init(font: KabanFont) {
         self.weight = font.weight
+        self.design = font.design.swiftUIFontDesign
         _scaledSize = .init(wrappedValue: font.size, relativeTo: font.relativeTo)
     }
 
     func body(content: Content) -> some View {
-        content.font(.system(size: scaledSize, weight: weight))
+        content.font(.system(size: scaledSize, weight: weight, design: design))
     }
 }
